92e427c1b1a330cf451b5d8e7afb6fd2586f37af
dedmagic <dedmagic@gmail.com>
2022-12-13 08:22:35 +0500

    icons font wip

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { AppTitle } from "./AppTitle/AppTitle";
 
 export const AppHeader = () => {
   return (
     <>
-      <h1>AppHeader</h1>
+      <AppTitle />
     </>
   );
 };
---
+++ src/components/AppHeader/AppTitle/AppTitle.tsx
@@ -0,0 +1,10 @@
+import "./AppTitle.css";
+
+export const AppTitle = () => {
+  return (
+    <>
+      <i className="mdi-book-open-outline"></i>
+      <h3 className="title">Повеление</h3>
+    </>
+  );
+};
