8b7d98c5612c84beddcc2dce3415bf28e1bf6eca
dedmagic <dedmagic@gmail.com>
2022-12-14 18:10:01 +0500

    wip

--- src/components/AppHeader/AppTitle/AppTitle.tsx
+++ src/components/AppHeader/AppTitle/AppTitle.tsx
@@ -3,8 +3,13 @@ import "./AppTitle.css";
 export const AppTitle = () => {
   return (
     <>
-      <i className="mdi-book-open-outline"></i>
-      <h3 className="title">Повеление</h3>
+      <h3 className="title">
+        {/* <span className="fa-regular fa-user icon"></span> */}
+        {/* <span className="fa-solid fa-user icon"></span> */}
+        {/* <span className="fa-brands fa-user icon"></span> */}
+        <i className="fa-brands fa-font-awesome icon"></i>
+        Повеление
+      </h3>
     </>
   );
 };
