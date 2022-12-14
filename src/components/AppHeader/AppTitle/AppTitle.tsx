5101f0f0f81d3f938baf7394f9083618da9accea
dedmagic <dedmagic@gmail.com>
2022-12-14 19:41:29 +0500

    wip

--- src/components/AppHeader/AppTitle/AppTitle.tsx
+++ src/components/AppHeader/AppTitle/AppTitle.tsx
@@ -1,22 +1,9 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./AppTitle.css";
 
 export const AppTitle = () => {
   return (
     <>
-<<<<<<< HEAD
-      <h3 className="title">
-        {/* <span className="fa-regular fa-user icon"></span> */}
-        {/* <span className="fa-solid fa-user icon"></span> */}
-        {/* <span className="fa-brands fa-user icon"></span> */}
-        <i className="fa-brands fa-font-awesome icon"></i>
-        Повеление
-      </h3>
-=======
-      {/* <i className="cup-togo"></i> */}
-      <FontAwesomeIcon icon="cup-togo" />
-      <h3 className="title">Повеление</h3>
->>>>>>> 5e32f5679fdce89923a908700ddde226b6e0ccbd
+      <h3 className="title fa-regular fa-hand-point-right">Повеление</h3>
     </>
   );
 };
