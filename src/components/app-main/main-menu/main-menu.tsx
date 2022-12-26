71a16cdfbca57b9cafe9a492642c4a9c08500ce6
dedmagic <dedmagic@gmail.com>
2022-12-26 07:18:32 +0500

    wip

--- src/components/app-main/main-menu/main-menu.tsx
+++ src/components/app-main/main-menu/main-menu.tsx
@@ -1,14 +1,37 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import "./main-menu.css";
 
 export const MainMenu = () => {
   return (
-    <ul>
-      <li>Поручения</li>
-      <li>Сотрудники</li>
-      <li>Должности</li>
-      <li>Аналитика</li>
-      <li>Настройки</li>
-      <li>Помощь</li>
-    </ul>
+    <div className="main-menu">
+      <ul className="business-logic-menu">
+        <li>
+          <a href="#">Поручения</a>
+        </li>
+        <li>
+          <a href="#">Сотрудники</a>
+        </li>
+        <li>
+          <a href="#">Должности</a>
+        </li>
+        <li>
+          <a href="#">Аналитика</a>
+        </li>
+      </ul>
+      <ul className="support-menu">
+        <li>
+          <a href="#">
+            <i className="fa fa-gear"></i>
+            Настройки
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i className="fa-regular fa-circle-question"></i>
+            Помощь
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 };
