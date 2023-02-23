029584411621577595a4efbd0046bf03e6351279
dedmagic <dedmagic@gmail.com>
2023-02-23 15:11:36 +0500

    wip

--- src/hooks/use-toggle.ts
+++ src/hooks/use-toggle.ts
@@ -4,20 +4,13 @@ export const useToggle = (
   initialValue: boolean = false
 ): [boolean, (value?: boolean) => void] => {
   const [toggle, setToggle] = useState<boolean>(initialValue);
-  const toggler = useCallback(
-    (value?: any) => {
-      // const newValue = typeof value === "boolean" ? value : !toggle;
-      // setToggle(newValue);
+  const toggler = useCallback((value?: any) => {
+    if (typeof value === "boolean") {
+      setToggle(value);
+      return;
+    }
 
-      if (typeof value === "boolean") {
-        setToggle(value);
-        return;
-      }
-
-      setToggle((oldValue) => !oldValue);
-    },
-    // [toggle]
-    []
-  );
+    setToggle((oldValue) => !oldValue);
+  }, []);
   return [toggle, toggler];
 };
