c57c9c27d64d8c9cfaa23b2038ec051bbb85b7d9
Oleg Ishchenko <dedmagic@gmail.com>
2023-02-24 14:32:05 +0500

    useToggle refactoring (#50)

--- src/hooks/use-toggle.ts
+++ src/hooks/use-toggle.ts
@@ -5,12 +5,7 @@ export const useToggle = (
 ): [boolean, (value?: boolean) => void] => {
   const [toggle, setToggle] = useState<boolean>(initialValue);
   const toggler = useCallback((value?: any) => {
-    if (typeof value === "boolean") {
-      setToggle(value);
-      return;
-    }
-
-    setToggle((oldValue) => !oldValue);
+    setToggle((oldValue) => (typeof value === "boolean" ? value : !oldValue));
   }, []);
   return [toggle, toggler];
 };
