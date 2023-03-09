f8f66960ce35b29b41064659ea2c8c50b17b0851
Oleg Ishchenko <dedmagic@gmail.com>
2023-03-09 08:17:33 +0500

    Validation result type (#62)
    * refactoring: type for validation result
    
    * small refactoring: css for card

--- src/models/position/types.ts
+++ src/models/position/types.ts
@@ -1,4 +1,4 @@
-import { Nullable } from "types";
+import { Nullable } from "types/types";
 
 export class Position {
   id: number = 0;
---
+++ src/types/types.ts
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+import { NO_ERRORS } from "./consts";
+
+export type Nullable<T> = T | null;
+
+export type EntityActionHandlerById = (id: number) => void;
+export type EntityActionHandlerByEntity<T> = (entity: T) => void;
+
+export type RenderFunction = (row?: any) => ReactNode;
+
+export type ValidationResult = string[] | typeof NO_ERRORS;
